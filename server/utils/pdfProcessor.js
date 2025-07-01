a1ea7d4150928938bec450be040d49319539ed63
Deepak Verma <deepak.verma.met20@itbhu.ac.in>
2025-07-01 11:26:45 +0530

    Update pdfProcessor.js

--- server/utils/pdfProcessor.js
+++ server/utils/pdfProcessor.js
@@ -1,13 +1,12 @@
-const fs = require('fs');
 const pdfParse = require('pdf-parse');
 
-async function extractTextFromPdf(filePath) {
-  const buffer = fs.readFileSync(filePath);
-  const data = await pdfParse(buffer);
+async function extractTextFromPDF(fileBuffer) {
+  const data = await pdfParse(fileBuffer);
 
   const MAX_CHARS = 15000; // ~5000 tokens
   const truncated = data.text.length > MAX_CHARS ? data.text.slice(0, MAX_CHARS) : data.text;
 
   return truncated;
 }
+
 module.exports = { extractTextFromPDF };
